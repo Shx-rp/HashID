c5901a7907797e4845ecbcd16548a681fd89f8b0
Shx-rp <45079643+Shx-rp@users.noreply.github.com>
2022-10-26 20:32:42 +0100

    Add files via upload

--- hashID.py
+++ hashID.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 def main():
 
@@ -6,30 +7,37 @@ def main():
 
     hashSalt = ":"
 
-    hashSaltCheck = hash.split(hashSalt, 1) [0]
+    hashSaltCheck = hash.split(hashSalt) [0]
     hashEncoded = hashSaltCheck.encode("utf-8")
+    #print(len(hashEncoded)*4)
 
-    
+    print("-------------------------------------\n")
     if len(hashEncoded)*4 == 128 and bytes.isupper(hashEncoded) == False :
-        print("MD5 Hash Detected!!")
+        print("|MD5 Hash Detected !!\n")
+        if len(hash) > len(hashEncoded):
+            print("|Salted/Passed Hash\n")
+            salt = hash.split(hashSalt) [1]
+            print("|Salt/Pass : " + salt)
 
     elif len(hashEncoded)*4 == 160:
-        print("SHA1 Hash Detected!!")
+        print("|SHA1 Hash Detected !!")
 
     elif len(hashEncoded)*4 == 128 and  bytes.isupper(hashEncoded) == True :
-        print("NTLM Hash Detected!!")
+        print("|NTLM Hash Detected !!")
 
     elif len(hashEncoded)*4 == 224:
-        print("SHA-224 Hash Detected!!")
+        print("|SHA-224 Hash Detected !!")
     
     elif len(hashEncoded)*4 == 256:
-        print("SHA-256 Hash Detected!!")
+        print("|SHA-256 Hash Detected !!")
 
     elif len(hashEncoded)*4 == 512:
-        print("SHA-512 Hash Detected")
+        print("|SHA-512 Hash Detected !!")
+    
+    if hash.find("$BLAKE2$") == 0:
+        print("|BLAKE2b-512 Hash Detected !!")
 
-    if len(hash) > len(hashEncoded):
-        print("Possible Salted/Passed Hash!!")
+    print("\n-------------------------------------")
 
     
 if __name__ == "__main__":
